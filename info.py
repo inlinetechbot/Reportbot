66b4c2e72afe72312c8365f2a721aaa5ffc663c9
inlinetechbot <inlinetechbot@gmail.com>
2026-09-06 15:15:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,7 +9,7 @@ class Config(object):
     
     # Other Configs
     BOT_START_TIME = time.time()
-    OWNER = int(os.environ.get("OWNER", "8462504573")) # ⚠️ Required
+    OWNER = int(os.environ.get("OWNER", "7185778863")) # ⚠️ Required
 
     # Web Response Config
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
